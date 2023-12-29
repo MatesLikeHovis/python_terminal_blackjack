2cc5e9d8e93445aed34ba5594d1b727fe5403be9
Brian Bunker <bbunk001@ucr.edu>
2023-12-28 17:54:21 -0800

    foundational classes added

--- script.py
+++ script.py
@@ -1,1 +1,31 @@
-// this is the script.py file
+
+class Player:
+    def __init__(self, name, money, cards, cardValue):
+        self.name = name
+        self.money = money
+        self.cards = cards
+        self.cardValue = cardValue
+
+    def __repr__(self):
+        return f"Player {self.name} has ${self.money}, holds the {self.cards} cards with a value of {self.cardValue}"
+    
+    def addMoney(self, amount):
+        self.money += amount
+
+    def RemoveMoney(self, amount):
+        self.money -= amount
+
+    def EmptyCards(self):
+        self.cards = []
+        self.cardValue = 0
+
+class Card:
+    def __init__(self, name, suit, value):
+        self.name = name
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return f"A {self.name} of {self.suit}, worth {self.value}"
+    
+
